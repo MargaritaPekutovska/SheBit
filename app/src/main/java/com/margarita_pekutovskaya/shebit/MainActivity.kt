a31d0a76252756a7c5a01afb07438b02e0c4b38a
MargaritaPekutovska <bondarchuk.margarita@gmail.com>
2025-02-27 16:46:19 +0100

    Organized imports

--- app/src/main/java/com/margarita_pekutovskaya/shebit/MainActivity.kt
+++ app/src/main/java/com/margarita_pekutovskaya/shebit/MainActivity.kt
@@ -7,9 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.margarita_pekutovskaya.crypto_home.screen.CryptoHomeScreen
 import com.margarita_pekutovskaya.shebit.ui.theme.SheBitTheme
