f6d1d823d1c5877c2105433e04364e4143161ab0
MargaritaPekutovska <bondarchuk.margarita@gmail.com>
2025-03-05 13:21:30 +0100

    Add AuthenticationInterceptor and implement API client with Retrofit

--- api-crypto/src/main/java/com/margarita_pekutovskaya/api_crypto/CoinDataClient.kt
+++ api-crypto/src/main/java/com/margarita_pekutovskaya/api_crypto/CoinDataClient.kt
@@ -1,16 +1,22 @@
 package com.margarita_pekutovskaya.api_crypto
 
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class CoinDataClient {
 
     private fun getInstance(): Retrofit {
+        val client = OkHttpClient.Builder()
+            .addInterceptor(AuthenticationInterceptor())
+            .build()
+
         return Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(
                 GsonConverterFactory.create()
             )
+            .client(client)
             .build()
     }
 
