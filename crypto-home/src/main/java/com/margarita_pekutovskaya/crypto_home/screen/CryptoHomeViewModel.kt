b558695c9525710256f69b40847db33bedd444c5
MargaritaPekutovska <bondarchuk.margarita@gmail.com>
2025-03-21 15:05:14 +0100

    Implemented a new API endpoint for fetching market data Removed the obsolete Coin class, as it's no longer needed

--- crypto-home/src/main/java/com/margarita_pekutovskaya/crypto_home/screen/CryptoHomeViewModel.kt
+++ crypto-home/src/main/java/com/margarita_pekutovskaya/crypto_home/screen/CryptoHomeViewModel.kt
@@ -1,24 +1,32 @@
 package com.margarita_pekutovskaya.crypto_home.screen
 
 import android.util.Log
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
 import com.margarita_pekutovskaya.api_crypto.CoinDataClient
 import com.margarita_pekutovskaya.api_crypto.CoinDataRepository
+import com.margarita_pekutovskaya.api_crypto.MarketDataDetails
 import kotlinx.coroutines.launch
 
 class CryptoHomeViewModel(
     private val coinDataRepository: CoinDataRepository
 ) : ViewModel() {
 
+    var dataCoin by mutableStateOf<List<MarketDataDetails>> (emptyList())
+    private set
+
     init {
         viewModelScope.launch {
             try {
-                val result = coinDataRepository.getMarketDataDetails(
+                val result: List<MarketDataDetails> = coinDataRepository.getMarketDataDetails(
                     currency = "usd",
                     coinIds = CryptoCoin.getCoinIds()
                 )
+                dataCoin = result
                 Log.d("CryptoHomeViewModel", "Data fetched successfully: $result")
             } catch (e: Exception) {
                 Log.e("CryptoHomeViewModel", "Error fetching market data", e)
