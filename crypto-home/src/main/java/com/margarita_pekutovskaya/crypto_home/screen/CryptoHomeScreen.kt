3168187cb9d4f26c9309dd17dbd2ce2c2f30d1e3
MargaritaPekutovska <bondarchuk.margarita@gmail.com>
2025-04-03 13:49:09 +0200

    added icons for cryptos and new folder for res

--- crypto-home/src/main/java/com/margarita_pekutovskaya/crypto_home/screen/CryptoHomeScreen.kt
+++ crypto-home/src/main/java/com/margarita_pekutovskaya/crypto_home/screen/CryptoHomeScreen.kt
@@ -21,7 +21,10 @@ import androidx.compose.material3.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -29,26 +32,27 @@ import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.rememberAsyncImagePainter
 import com.margarita_pekutovskaya.api_crypto.MarketDataDetails
+import com.margarita_pekutovskaya.crypto_home.R
 
 @Composable
 fun CryptoHomeScreen(
     modifier: Modifier = Modifier,
     viewModel: CryptoHomeViewModel = viewModel(factory = CryptoHomeViewModel.Factory),
 ) {
-    Column (
+    Column(
         modifier = modifier
             .fillMaxSize()
             .background(
-                color = Color.Black
+                color = colorResource(R.color.black2)
             )
             .padding(16.dp),
         verticalArrangement = Arrangement.Top
-    ){
+    ) {
         Text(
-            text= "Watch List", //resourses
+            text = stringResource(R.string.CryptoHomeScreenText),
             fontSize = 24.sp,
             fontWeight = FontWeight.Bold,
-            color = Color.Gray,
+            color = colorResource(R.color.white),
             modifier = Modifier
                 .padding(bottom = 16.dp)
         )
@@ -63,20 +67,24 @@ fun CryptoHomeScreen(
 
 @Composable
 private fun CryptoItem(crypto: MarketDataDetails) {
-    Row (
+    Row(
         modifier = Modifier
             .fillMaxWidth()
             .padding(vertical = 8.dp)
-            .background(Color.LightGray, shape = RoundedCornerShape(10.dp))
+            .background(
+                color = colorResource(R.color.teal_200),
+                shape = RoundedCornerShape(10.dp)
+            )
             .padding(16.dp),
         verticalAlignment = Alignment.CenterVertically
-    ){
+    ) {
         Image(
-            painter = rememberAsyncImagePainter(crypto), //icon download
+            painter = rememberAsyncImagePainter(crypto.image),
             contentDescription = crypto.name,
             modifier = Modifier
                 .size(40.dp)
                 .clip(CircleShape)
+                .shadow(8.dp)
         )
 
         Spacer(modifier = Modifier.width(12.dp))
@@ -91,7 +99,7 @@ private fun CryptoItem(crypto: MarketDataDetails) {
             Text(
                 text = "${crypto.currentPrice} ${CurrencyType.USD}",
                 fontSize = 14.sp,
-                color = Color.Gray
+                color = colorResource(R.color.black)
             )
         }
     }
